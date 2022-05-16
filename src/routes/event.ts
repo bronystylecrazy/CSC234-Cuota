30ec1632d0c7f8cef4d5ce187975f4f78a4237a8
Sirawit Pratoomsuwan <48322519+bronystylecrazy@users.noreply.github.com>
2022-05-16 18:31:25 +0700

    uodate category routes

--- src/routes/event.ts
+++ src/routes/event.ts
@@ -2,6 +2,7 @@ import { Account, Event, EventJoin, Type } from "@/database/models";
 import deepsearch from "@/services/deepsearch";
 import { isLogin } from "@/services/Utils";
 import express, { Request } from "express";
+import moment from "moment";
 // eslint-disable-next-line new-cap
 const eventRoute = express.Router();
 
@@ -21,34 +22,44 @@ const eventRoute = express.Router();
 }
  */
 eventRoute.post("/", async (req, res) => {
-	// Logged In Check!
-	if (!isLogin(req)) {
-		return res.status(401).json({
-			message: "Unauthorized, you have to login first!",
+	try {
+		// Logged In Check!
+		if (!isLogin(req)) {
+			return res.status(401).json({
+				message: "Unauthorized, you have to login first!",
+				success: false,
+			});
+		}
+		const host_id = req.user.user_id;
+		const body = req.body;
+		console.log(body);
+		const newEvent = await Event.create({
+			host_id,
+			title: body.title || "",
+			detail: body.detail || "",
+			location: body.location || "",
+			gender: body.gender.toLowerCase() || "male",
+			eventDate:
+				new Date(moment(body.eventDate).toString()) || new Date(),
+			subType_id: body.subType_id || 0,
+			minAge: body.minAge || 18,
+			maxAge: body.maxAge || 65,
+			maxJoin: body.maxJoin || 20,
+			awesome: 0,
+		});
+
+		return res.json({
+			message: "Create event successful",
+			success: true,
+			data: newEvent,
+		});
+	} catch (e) {
+		return res.json({
 			success: false,
+			message: e.message,
+			data: {},
 		});
 	}
-	const host_id = req.user.user_id;
-	const body = req.body;
-	const newEvent = await Event.create({
-		host_id,
-		title: body.title || "",
-		detail: body.detail || "",
-		location: body.location || "",
-		gender: body.gender || "male",
-		eventDate: body.eventData || new Date(),
-		subType_id: body.subType_id || 0,
-		minAge: body.minAge || 18,
-		maxAge: body.maxAge || 65,
-		maxJoin: body.maxJoin || 20,
-		awesome: 0,
-	});
-
-	return res.json({
-		message: "Create event successful",
-		success: true,
-		data: newEvent,
-	});
 });
 
 async function getEvents(req: Request) {
@@ -86,106 +97,167 @@ async function getEvents(req: Request) {
 
 // GET /event to get all events.  EXPLORE
 eventRoute.get("/", async (req, res) => {
-	const { results } = await getEvents(req);
-	return res.json({
-		message:
-			results.length > 0
-				? "Get all events successful"
-				: "There are no events now, create one!",
-		success: true,
-		data: results,
-	});
+	try {
+		const { results } = await getEvents(req);
+		return res.json({
+			message:
+				results.length > 0
+					? "Get all events successful"
+					: "There are no events now, create one!",
+			success: true,
+			data: results,
+		});
+	} catch (e) {
+		return res.json({
+			success: false,
+			message: e.message,
+			data: {},
+		});
+	}
 });
 
 // GET /event to get all events.  EXPLORE
 eventRoute.get("/explore", async (req, res) => {
-	const { results, categories: subcategories } = await getEvents(req);
-	const categories = {};
-	for (const category of subcategories) {
-		if (!categories[category.mainType]) {
-			categories[category.mainType] = {};
-		}
+	try {
+		const { results, categories: subcategories } = await getEvents(req);
+		const categories = {};
+		for (const category of subcategories) {
+			if (!categories[category.mainType]) {
+				categories[category.mainType] = {};
+			}
 
-		if (!categories[category.mainType][category.subType]) {
-			categories[category.mainType][category.subType] = [];
-		}
+			if (!categories[category.mainType][category.subType]) {
+				categories[category.mainType][category.subType] = [];
+			}
 
-		categories[category.mainType][category.subType] = results.filter(
-			(event) => event.subType_id === category.id
-		);
+			categories[category.mainType][category.subType] = results.filter(
+				(event) => event.subType_id === category.id
+			);
+		}
+		// results.forEach((result) => {});
+
+		return res.json({
+			message:
+				results.length > 0
+					? "Get all events successful"
+					: "There are no events now, create one!",
+			success: true,
+			data: categories,
+		});
+	} catch (e) {
+		return res.json({
+			success: false,
+			message: e.message,
+			data: {},
+		});
 	}
-	// results.forEach((result) => {});
-
-	return res.json({
-		message:
-			results.length > 0
-				? "Get all events successful"
-				: "There are no events now, create one!",
-		success: true,
-		data: categories,
-	});
 });
 
 // GET /event to get all events.  EXPLORE
 eventRoute.get("/explore/random", async (req, res) => {
-	const { results, categories: subcategories } = await getEvents(req);
-	const categories = {};
-	for (const category of subcategories) {
-		if (!categories[category.mainType]) {
-			categories[category.mainType] = {};
-		}
+	try {
+		const { results, categories: subcategories } = await getEvents(req);
+		const categories = {};
+		for (const category of subcategories) {
+			if (!categories[category.mainType]) {
+				categories[category.mainType] = {};
+			}
 
-		if (!categories[category.mainType][category.subType]) {
-			categories[category.mainType][category.subType] = [];
-		}
+			if (!categories[category.mainType][category.subType]) {
+				categories[category.mainType][category.subType] = [];
+			}
 
-		categories[category.mainType][category.subType] = results.filter(
-			(event) => event.subType_id === category.id
-		);
-		categories[category.mainType] =
-			categories[category.mainType][category.subType][
-				Math.floor(
-					Math.random() *
-						categories[category.mainType][category.subType].length
-				)
-			] || null;
+			categories[category.mainType][category.subType] = results.filter(
+				(event) => event.subType_id === category.id
+			);
+			categories[category.mainType] =
+				categories[category.mainType][category.subType][
+					Math.floor(
+						Math.random() *
+							categories[category.mainType][category.subType]
+								.length
+					)
+				] || null;
+		}
+		// results.forEach((result) => {});
+
+		return res.json({
+			message:
+				results.length > 0
+					? "Get all events successful"
+					: "There are no events now, create one!",
+			success: true,
+			data: categories,
+		});
+	} catch (e) {
+		return res.json({
+			success: false,
+			message: e.message,
+			data: {},
+		});
 	}
-	// results.forEach((result) => {});
+});
 
-	return res.json({
-		message:
-			results.length > 0
-				? "Get all events successful"
-				: "There are no events now, create one!",
-		success: true,
-		data: categories,
-	});
+eventRoute.get("/category", async (req, res) => {
+	try {
+		const [subcategories]: [any[]] = await Promise.all([Type.findAll({})]);
+		const categoryMap = {};
+		for (const category of subcategories) {
+			if (!categoryMap[category.mainType]) {
+				categoryMap[category.mainType] = [];
+			}
+			categoryMap[category.mainType].push({
+				id: category.id,
+				subType: category.subType,
+			});
+		}
+
+		return res.json({
+			message: "Get all categories successful!",
+			success: true,
+			data: categoryMap,
+		});
+	} catch (e) {
+		return res.json({
+			success: false,
+			message: e.message,
+			data: {},
+		});
+	}
 });
 
 // GET /event/:id to get by category.  EXPLORE
 eventRoute.get("/:subType_id", async (req, res) => {
-	const subType_id = parseInt(req.params.subType_id);
+	try {
+		const subType_id = parseInt(req.params.subType_id);
 
-	if (!isLogin(req)) {
+		if (!isLogin(req)) {
+			return res.json({
+				message: "Unauthorized, you have to login first!",
+				success: false,
+			});
+		}
+
+		const events = await Event.findAll({
+			where: {
+				subType_id,
+			},
+		});
 		return res.json({
-			message: "Unauthorized, you have to login first!",
+			message:
+				events.length > 0
+					? "Get all events successful!"
+					: "There're no events in this category!",
+			success: true,
+			data: events,
+		});
+	} catch (e) {
+		return res.json({
 			success: false,
+			message: e.message,
+			data: {},
 		});
 	}
-
-	const events = await Event.findAll({
-		where: {
-			subType_id,
-		},
-	});
-	return res.json({
-		message:
-			events.length > 0
-				? "Get all events successful!"
-				: "There're no events in this category!",
-		success: true,
-		data: events,
-	});
 });
 
 export default eventRoute;
